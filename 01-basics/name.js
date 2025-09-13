9057e433304c8f783b7381348553cb6983c7681f
Mohammed Aeraf <aeraf@outlook.com>
2025-09-13 20:19:06 +0530

    loops and patterns

---
+++ 01-basics/name.js
@@ -0,0 +1,4 @@
+let firstName = "Abdullaah";
+let lastName = "Shaikh";
+let fullName = firstName + " " + lastName;
+console.log(fullName);
--- 01-basics/tables.js
+++ 01-basics/tables.js
@@ -1,5 +1,8 @@
-let n = 2;
-for (let i = 1; i <= 10; i++) {
-  let result = n * i;
-  console.log(n + " x " + i + " = " + result);
+for (let n = 1; n <= 10; n++) {
+  console.log("Printing Multiplication table of " + n);
+  for (let i = 1; i <= 10; i++) {
+    let result = n * i;
+    console.log(n + " x " + i + " = " + result);
+  }
+  console.log();
 }
